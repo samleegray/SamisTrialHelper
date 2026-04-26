ffc2a70c9bc6d29f5573bc3580c0d1fe1ee93966
Sam Gray <samleegray@gmail.com>
2026-04-26 00:25:08 -0700

    Initiate SamisTrialHelper addon metadata and structure
    Set up the foundational structure for SamisTrialHelper, including metadata attributes such as name, displayName, version, and author styling. This lays the groundwork for further development and integration.

--- SamisTrialHelper.lua
+++ SamisTrialHelper.lua
@@ -1,3 +1,13 @@
+SamisTrialHelperAddon = SamisTrialHelperAddon or {}
+
+local STH = SamisTrialHelperAddon
+
+STH.name = "SamisTrialHelper"
+STH.displayName = "Sami's Trial Helper"
+STH.version = "0.0.1"
+STH.author = "samihaize"
+STH.styledAuthor = "|cf500e2s|r|ceb00e5a|r|ce100e9m|r|cd700edi|r|cce00f0h|r|cc400f4a|r|cba00f8i|r|cb000fbz|r|ca600ffe|r"
+
 local STH = SamisTrialHelperAddon
 local SAMID = SamisTrialAddonsDebugHelpers
 
